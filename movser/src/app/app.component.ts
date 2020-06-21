1b298bb5884b43d34ed81f4ee52f7ada2a3f54c5
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2020-06-21 10:28:48 +0200

    added dark mode toggle

--- movser/src/app/app.component.ts
+++ movser/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -7,9 +7,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  private toggle;
+  private prefersDark;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -17,11 +19,34 @@ export class AppComponent {
   ) {
     this.initializeApp();
   }
+  ngAfterViewInit(): void {
+    // Query for the toggle that is used to change between themes
+    this.toggle = document.querySelector('#themeToggle');
+    this.prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.toggle.addEventListener('ionChange', (ev) => {
+      document.body.classList.toggle('dark', ev.detail.checked);
+    });
+    // Listen for changes to the prefers-color-scheme media query
+    this.prefersDark.addListener((e) => this.checkToggle(e.matches));
+
+    this.loadApp();
+  }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  // Called when the app loads
+  private loadApp(): void {
+    this.checkToggle(this.prefersDark.matches);
+  }
+
+  // Called by the media query to check/uncheck the toggle
+  private checkToggle(shouldCheck) {
+    this.toggle.checked = shouldCheck;
   }
 }
