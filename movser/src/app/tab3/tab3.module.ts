6f181e241d2fa6ab0037248549b5e3d3eca9dd39
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2020-06-21 12:34:43 +0200

    about tab

--- movser/src/app/tab3/tab3.module.ts
+++ movser/src/app/tab3/tab3.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module'
 import { SharedModule } from '../shared/shared.module';
@@ -14,7 +13,6 @@ import { SharedModule } from '../shared/shared.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab3PageRoutingModule,
     SharedModule
   ],
