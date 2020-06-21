fe1a0fe28b3a2047d0f09452c49eb0d9f9d4a848
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2020-06-21 10:35:01 +0200

    renamed tabs

--- movser/src/app/tabs/tabs-routing.module.ts
+++ movser/src/app/tabs/tabs-routing.module.ts
@@ -4,19 +4,19 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'app',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'movies',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'tv-series',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'about',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
@@ -28,7 +28,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/app/movies',
     pathMatch: 'full'
   }
 ];
